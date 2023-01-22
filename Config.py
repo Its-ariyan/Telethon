46df1cabcf8b89a471a23e60afd7de98288e7e9c
❥︎ ɪᴛs'ᴀʀɪʏᴀɴ <115484197+Its-ariyan@users.noreply.github.com>
2023-01-22 23:25:11 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,8 +4,9 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    OWNER_ID = int(getenv("OWNER_ID", "5761513990"))
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", "ENABLE")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
@@ -14,4 +15,4 @@ class Config(object):
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
+    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', True) # Change it to "True"
