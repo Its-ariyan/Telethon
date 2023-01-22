31905ab6e9cef27ad3b7d5d6d62d8439c22cc00c
❥︎ ɪᴛs'ᴀʀɪʏᴀɴ <115484197+Its-ariyan@users.noreply.github.com>
2023-01-22 23:14:24 +0600

    Update admins.py

--- Zaid/plugins/admins.py
+++ Zaid/plugins/admins.py
@@ -17,21 +17,46 @@ async def _(event):
 
     await event.edit(PLAY_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
 
+@Zaid.on(events.callbackquery.CallbackQuery(data="clone"))
+async def _(event):
+
+    await event.edit(CLONE_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
+
 
 ADMIN_TEXT = """
 **✘ A module from which admins of the chat can use!**
 
-‣ `?end` - To End music streaming.
-‣ `?skip` - To Skip Tracks Going on.
-‣ `?pause` - To Pause streaming.
-‣ `?resume` - to Resume Streaming.
-‣ `?leavevc` - force The Userbot to leave Vc Chat (Sometimes Joined).
-‣ `?playlist` - to check playlists.
+‣ /end - To End music streaming.
+‣ /skip - To Skip Tracks Going on.
+‣ /pause - To Pause streaming.
+‣ /resume - to Resume Streaming.
+‣ /leavevc - force The Userbot to leave Vc Chat (Sometimes Joined).
+‣ /playlist - to check playlists.
 """
 
 PLAY_TEXT = """
 **✘ A module from which users of the chat can use!**
 
-‣ `?play` - To Play Audio from Else Reply to audio file.
-‣ `?vplay` - To Stream Videos (HEROKU_MODE > Doesn't support).
+‣ /play - To Play Audio from Else Reply to audio file.
+‣ /vplay - To Stream Videos (HEROKU_MODE > Doesn't support).
+"""
+
+CLONE_TEXT = """
+ᴄʟᴏɴᴇ ʏᴏᴜʀ ʙᴏᴛ 
+
+ᴛʜᴇʏ ʜᴀᴠᴇ ᴛʜᴇ sᴀᴍᴇ ᴀʙɢ ʙᴏᴛ ғᴜɴᴄᴛɪᴏɴs, ᴜᴘᴅᴀᴛᴇs ᴀɴᴅ ᴅᴀᴛᴀʙᴀsᴇ.
+
+ᴛᴏ ᴄʀᴇᴀᴛᴇ ᴀ ᴄʟᴏɴᴇ:
+• ɢᴏ ᴛᴏ @BotFather
+• sᴛᴀʀᴛ ɪᴛ ᴀɴᴅ ᴛʏᴘᴇ /newbot
+• ᴄʜᴏᴏsᴇ ʏᴏᴜʀ ᴄʟᴏɴᴇ's ɴᴀᴍᴇ
+• ᴄʜᴏᴏsᴇ ʏᴏᴜʀ ᴄʟᴏɴᴇ's ᴜsᴇʀɴᴀᴍᴇ
+
+• sᴇɴᴅ ᴛʜᴇ ʙᴏᴛғᴀᴛʜᴇʀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ᴀᴘɪ ᴋᴇʏ ᴛᴏ @SankiXClonerBot 
+ ( ᴍᴜsᴛ ʙᴇ ғᴏʀᴡᴀʀᴅ)
+ 
+- ᴀғᴛᴇʀ sᴏᴍᴇ ʜᴏᴜʀ ᴛʜᴇɴ sᴇʀᴠᴇʀ ʀᴇsᴛᴀʀᴛ ᴡɪʟʟ ʙᴇ ᴀɴᴅ ᴄʟᴇᴀʀ ᴏʟᴅ ᴀʟʟ ᴅᴀᴛᴀ ᴛʜᴇɴ ʏᴏᴜʀ ʙᴏᴛ sᴛᴏᴘ!
+sᴏ ᴛʜᴇɴ ᴀɢᴀɪɴ ғᴏʀᴡᴀʀᴅ ʏᴏᴜʀ ʙᴏᴛ ᴛᴏᴋᴇɴ 
+
+• ᴅᴏɴᴇ! ɴᴏᴡ ᴇɴᴊᴏʏ ʏᴏᴜʀ ʙᴏᴛ 👍
 """
